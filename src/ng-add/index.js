ee538b32f70c0ea749c8c7dadf93b97f75dc7685
andy23512 <andy23512@gmail.com>
2020-03-22 14:53:51 +0800

    Handle initializer logic

--- src/ng-add/index.js
+++ src/ng-add/index.js
@@ -1,14 +1,55 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const schematics_1 = require("@angular-devkit/schematics");
 const config_1 = require("@schematics/angular/utility/config");
-const schematics_1 = require("@angular/cdk/schematics");
+const schematics_2 = require("@angular/cdk/schematics");
+const ts = require("typescript");
+const ast_utils_1 = require("@schematics/angular/utility/ast-utils");
+function readIntoSourceFile(host, fileName) {
+    const buffer = host.read(fileName);
+    if (buffer === null) {
+        throw new schematics_1.SchematicsException(`File ${fileName} does not exist.`);
+    }
+    return ts.createSourceFile(fileName, buffer.toString('utf-8'), ts.ScriptTarget.Latest, true);
+}
 function default_1(options) {
     return (tree, context) => {
         const workspace = config_1.getWorkspace(tree);
-        const project = schematics_1.getProjectFromWorkspace(workspace, options.project);
-        schematics_1.addModuleImportToRootModule(tree, 'HttpClientModule', '@angular/common/http', project);
-        schematics_1.addModuleImportToRootModule(tree, `HttpClientXsrfModule.withOptions({cookieName: '${options.akimboProjectName}-csrf', headerName: 'X-CSRFToken'})`, '@angular/common/http', project);
+        const project = schematics_2.getProjectFromWorkspace(workspace, options.project);
+        const appModulePath = schematics_2.getAppModulePath(tree, schematics_2.getProjectMainFile(project));
+        const sourceFile = readIntoSourceFile(tree, appModulePath);
+        schematics_2.addModuleImportToRootModule(tree, 'HttpClientModule', '@angular/common/http', project);
+        schematics_2.addModuleImportToRootModule(tree, `HttpClientXsrfModule.withOptions({cookieName: '${options.akimboProjectName}-csrf', headerName: 'X-CSRFToken'})`, '@angular/common/http', project);
         context.logger.info('✅️ Import HttpClientModule into root module');
+        const change = schematics_2.insertImport(sourceFile, appModulePath, 'HttpClient', '@angular/common/http');
+        const recorder = tree.beginUpdate(appModulePath);
+        recorder.insertLeft(change.pos, change.toAdd);
+        // 先抓到所有的 ImportDeclaration
+        const allImports = sourceFile.statements.filter(node => ts.isImportDeclaration(node));
+        // 找到最後一個 ImportDeclaration
+        let lastImport;
+        for (const importNode of allImports) {
+            if (!lastImport || importNode.getStart() > lastImport.getStart()) {
+                lastImport = importNode;
+            }
+        }
+        // 準備好要插入的程式碼
+        const importStr = `\nexport function getAuthSettings(http: HttpClient) {
+return () =>
+  http
+    .get('/api/get_csrf')
+    .toPromise();
+}
+`;
+        // 在最後一個 ImportDeclaration 結束的位置插入程式碼
+        recorder.insertLeft(lastImport.end, importStr);
+        tree.commitUpdate(recorder);
+        ast_utils_1.addProviderToModule(sourceFile, appModulePath, `{
+      provide: APP_INITIALIZER,
+      useFactory: getAuthSettings,
+      multi: true,
+      deps: [HttpClient],
+    }`, '@angular/core');
         return tree;
     };
 }
--- src/ng-add/index.ts
+++ src/ng-add/index.ts
@@ -1,15 +1,43 @@
-import { Rule, Tree, SchematicContext } from '@angular-devkit/schematics';
+import {
+  Rule,
+  Tree,
+  SchematicContext,
+  SchematicsException
+} from '@angular-devkit/schematics';
 import { getWorkspace } from '@schematics/angular/utility/config';
 import {
   getProjectFromWorkspace,
-  addModuleImportToRootModule
+  addModuleImportToRootModule,
+  getAppModulePath,
+  getProjectMainFile,
+  insertImport
 } from '@angular/cdk/schematics';
 import { Schema } from './schema';
+import { InsertChange } from '@schematics/angular/utility/change';
+import { SourceFile } from 'typescript';
+import * as ts from 'typescript';
+import { addProviderToModule } from '@schematics/angular/utility/ast-utils';
+
+function readIntoSourceFile(host: Tree, fileName: string): SourceFile {
+  const buffer = host.read(fileName);
+  if (buffer === null) {
+    throw new SchematicsException(`File ${fileName} does not exist.`);
+  }
+
+  return ts.createSourceFile(
+    fileName,
+    buffer.toString('utf-8'),
+    ts.ScriptTarget.Latest,
+    true
+  );
+}
 
 export default function(options: Schema): Rule {
   return (tree: Tree, context: SchematicContext) => {
     const workspace = getWorkspace(tree);
     const project = getProjectFromWorkspace(workspace, options.project);
+    const appModulePath = getAppModulePath(tree, getProjectMainFile(project));
+    const sourceFile = readIntoSourceFile(tree, appModulePath);
     addModuleImportToRootModule(
       tree,
       'HttpClientModule',
@@ -23,6 +51,55 @@ export default function(options: Schema): Rule {
       project
     );
     context.logger.info('✅️ Import HttpClientModule into root module');
+    const change = insertImport(
+      sourceFile as any,
+      appModulePath,
+      'HttpClient',
+      '@angular/common/http'
+    ) as InsertChange;
+
+    const recorder = tree.beginUpdate(appModulePath);
+    recorder.insertLeft(change.pos, change.toAdd);
+
+    // 先抓到所有的 ImportDeclaration
+    const allImports = sourceFile.statements.filter(node =>
+      ts.isImportDeclaration(node)
+    )! as ts.ImportDeclaration[];
+
+    // 找到最後一個 ImportDeclaration
+    let lastImport: ts.Node | undefined;
+    for (const importNode of allImports) {
+      if (!lastImport || importNode.getStart() > lastImport.getStart()) {
+        lastImport = importNode;
+      }
+    }
+
+    // 準備好要插入的程式碼
+    const importStr = `\nexport function getAuthSettings(http: HttpClient) {
+return () =>
+  http
+    .get('/api/get_csrf')
+    .toPromise();
+}
+`;
+
+    // 在最後一個 ImportDeclaration 結束的位置插入程式碼
+    recorder.insertLeft(lastImport!.end, importStr);
+
+    tree.commitUpdate(recorder);
+
+    addProviderToModule(
+      sourceFile,
+      appModulePath,
+      `{
+      provide: APP_INITIALIZER,
+      useFactory: getAuthSettings,
+      multi: true,
+      deps: [HttpClient],
+    }`,
+      '@angular/core'
+    );
+
     return tree;
   };
 }
