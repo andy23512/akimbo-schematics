6f7fbcba22eb12ed7f2ca76879cc125334818cf4
andy23512 <andy23512@gmail.com>
2020-03-22 11:40:50 +0800

    Try to add import module logic

--- src/ng-add/index.ts
+++ src/ng-add/index.ts
@@ -1,8 +1,33 @@
-import { Rule, Tree, SchematicContext } from '@angular-devkit/schematics';
+import {
+  Rule,
+  Tree,
+  SchematicContext,
+  SchematicsException
+} from '@angular-devkit/schematics';
+import { getWorkspace } from '@schematics/angular/utility/config';
+import {
+  getProjectFromWorkspace,
+  addModuleImportToRootModule
+} from '@angular/cdk/schematics';
 import { Schema } from './schema';
 
 export default function(options: Schema): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
+  return (tree: Tree, context: SchematicContext) => {
+    const workspace = getWorkspace(tree);
+    const project = getProjectFromWorkspace(workspace, options.project);
+    addModuleImportToRootModule(
+      tree,
+      'HttpClientModule',
+      '@angular/common/http',
+      project
+    );
+    addModuleImportToRootModule(
+      tree,
+      `HttpClientXsrfModule.withOptions({cookieName: '${options.akimboProjectName}-csrf', headerName: 'X-CSRFToken'})`,
+      '@angular/common/http',
+      project
+    );
+    context.logger.info('✅️ Import HttpClientModule into root module');
     return tree;
   };
 }
